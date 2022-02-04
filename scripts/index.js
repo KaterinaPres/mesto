09f34a2786c81177eaabde579b20bd30988f25e0
Ekaterina Presnyakova <katuwka1@mail.ru>
2022-02-04 21:54:51 +0400

    5PR_nachalo

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,108 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 let profileOpenPopupButton = document.querySelector('.profile__button');
-let popup = document.querySelector('.popup');
-let profileCloseButton = document.querySelector('.popup__close');
+let popupAdd = document.querySelector('.popup__add');
+let popupEdit = document.querySelector('.popup__edit');
+let profileCloseButton = document.querySelector('.popup__edit .popup__close');
+let template;
+let list = document.querySelector('.elements');
+let AddOpenPopupButton = document.querySelector('.profile__add-button');
+let AddCloseButton = document.querySelector('.popup__add .popup__close');
+
+let formElement = document.querySelector('form[name="popup-edit"]');
+let formElement1 = document.querySelector('form[name="popup-add"]');
+let namePersonag = document.getElementById('namePersonag');
+let professia = document.getElementById('professia');
+let profileNewName = document.querySelector('.profile__title');
+let profileNewProf = document.querySelector('.profile__subtitle');
+let AddNewName = document.querySelector('.element__text');
+let AddNewImage = document.querySelector('.element__image');
+let nameElement = document.getElementById('nameElement');
+let linkImage = document.getElementById('linkImage');
+
+profileOpenPopupButton.addEventListener('click', openPopupProfile);
+profileCloseButton.addEventListener('click', closePopup);
+AddOpenPopupButton.addEventListener('click', openPopupAdd);
+AddCloseButton.addEventListener('click', closePopup);
 
 
-profileOpenPopupButton.addEventListener('click', openPopup)
-profileCloseButton.addEventListener('click', closePopup)
+function render() {
+    initialCards.forEach(renderItem);
+}
+function init() {
+    template = document.querySelector('#template').content;
+    render();
+}
 
-let formElement = document.querySelector('form[name="popup-edit"]')
-let namePersonag = document.getElementById('namePersonag')
-let professia = document.getElementById('professia')
-let profileNewName = document.querySelector('.profile__title')
-let profileNewProf = document.querySelector('.profile__subtitle')
+function renderItem(element) {
+    const NewItem = template.cloneNode(true);
+    NewItem.querySelector('.element__text').innerText = element.name;
+    NewItem.querySelector('.element__image').setAttribute('src', element.link);
+    list.appendChild(NewItem);
+    addEventListener(NewItem);
+}
 
-function openPopup() {
-    namePersonag.value = profileNewName.innerHTML
-    professia.value = profileNewProf.innerHTML
-    popup.classList.add('popup_opened')
+function addEventListener(el) {
+    el.querySelector('.element__delete').addEventListener('click', handleDelete);
+}
+
+function handleDelete() {
+
+}
+
+function openPopupProfile() {
+    namePersonag.value = profileNewName.innerHTML;
+    professia.value = profileNewProf.innerHTML;
+    popupEdit.classList.add('popup_opened');
+}
+
+function openPopupAdd() {
+    popupAdd.classList.add('popup_opened');
 }
 
 function closePopup() {
-    popup.classList.remove('popup_opened')
+    popupEdit.classList.remove('popup_opened');
+    popupAdd.classList.remove('popup_opened');
 }
 
-function savePopup(evt) {
-    evt.preventDefault()
-    profileNewName.innerHTML = namePersonag.value
-    profileNewProf.innerHTML = professia.value
-    closePopup()
+function savePopupEdit(evt) {
+    evt.preventDefault();
+    profileNewName.innerHTML = namePersonag.value;
+    profileNewProf.innerHTML = professia.value;
+    closePopup();
 }
-formElement.addEventListener('submit', savePopup)
+
+function savePopupAdd(evt) {
+    evt.preventDefault();
+    AddNewName.innerHTML = nameElement.value;
+    AddNewImage.innerHTML = linkImage.value;
+    closePopup();
+}
+formElement.addEventListener('submit', savePopupEdit);
+formElement1.addEventListener('submit', savePopupAdd);
+
+document.addEventListener("DOMContentLoaded", init);
